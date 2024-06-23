99ae3cc2fb8eee0ef35ade8cc100b99214b541d3
Gaurav <gauravrawat0259@gmail.com>
2024-06-23 07:44:35 +0530

    Added 01vitereact and 02counter

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Demo from "./Demo"
+function App() {
+
+  return (
+    <>
+    <Demo/>
+    <h1>haha returniing multiple elememts</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Demo.jsx
@@ -0,0 +1,8 @@
+function Demo()
+{
+    return(
+        <h1>hello there ready ?</h1>
+    )
+}
+
+export default Demo
