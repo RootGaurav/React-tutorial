96a346ff01f144aa40c9ec2d86d240433e1e488c
Gaurav <gauravrawat0259@gmail.com>
2024-07-11 21:28:32 +0530

    Added 06currencyconvertor

---
+++ 06currencyconvertor/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/index.js",
+    "./src/components/inputBox.jsx",
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
