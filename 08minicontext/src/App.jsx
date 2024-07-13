f53741b76429d382d7190641201cdecf1dd72296
Gaurav <gauravrawat0259@gmail.com>
2024-07-13 17:02:25 +0530

    Added 08minicontext

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/login'
+import Profile from './components/profile'
+
+function App() {
+
+  return (
+
+
+    <UserContextProvider>
+      <h1>React</h1>
+      <Login/> 
+      <Profile/>
+
+    </UserContextProvider>
+     
+    
+  )
+}
+
+export default App
